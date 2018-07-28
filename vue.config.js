a4e1e9b2f280b9d6023377137f6d410dfd7ce17b
mmr.K <lmimsra07@gmail.com>
2018-07-28 14:55:50 +0900

    metaタグの設定を変更

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pwa: {
+        themeColor: '#4DBA87', // The Vue color
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'default',
+    }
+}
