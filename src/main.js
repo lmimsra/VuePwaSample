543602d3e495669b417b63dbe75639d314f7789f
mmr.K <lmimsra07@gmail.com>
2018-08-03 23:37:08 +0900

    firebaseモジュールの読み込み

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import './registerServiceWorker'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faCoffee, faUser} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {authConfig} from './config/firebaseConfig'
+import firebase from 'firebase'
 
 library.add(faCoffee,faUser);
 Vue.component('font-awesome-icon',FontAwesomeIcon);
 Vue.config.productionTip = false;
+firebase.initializeApp(authConfig);
 
 new Vue({
     router,
