07a910d7c8a228871cd865ae6540d77149fda783
mmr.K <lmimsra07@gmail.com>
2018-08-16 23:39:00 +0900

    ビンゴの値がそれっぽく出るようにスタイルを当てた

--- src/views/Bingo.vue
+++ src/views/Bingo.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-        今当たったのは・・・・<br/>
-        <div class="top-num">
-            <span id="topicon">{{bingoNum[0]}}!</span>
-        </div>
-        <h1>{{bingoNum}}</h1>
+        <p class="title">ビンゴ？(仮)</p>
+        <div v-if="bingoNum !== null">
+            今当たったのは・・・・<br/>
+            <div class="top-num">
+                <span id="topicon"><b>{{bingoNum[0]}}!</b></span>
+            </div>
+            <div v-for="(value,key) in bingoNum" v-bind:key="key">
 
+                <span class="title" v-show="key!==0">{{value}}</span>
+            </div>
+        </div>
+        <div v-else>
+            <h1 class="title">
+                準備中！<br/>
+                もう少々お待ちを！
+            </h1>
+            と言いつつ時々数字出ます
+        </div>
     </div>
 </template>
 
@@ -24,10 +36,11 @@
         created: function () {
             this.connection = firebase.database().ref('/bingo')
             this.connection.on('value', snapshot => {
-                this.bingoNum = snapshot.val().reverse()
+                this.bingoNum = snapshot.val()
+                if (this.bingoNum !== null) this.bingoNum.reverse()
                 this.len = this.bingoNum.length
-                console.log(this.bingoNum)
-                console.log(this.len)
+                // console.log(this.bingoNum)
+                // console.log(this.len)
 
             })
         }
@@ -41,8 +54,10 @@
         padding: 10px;
         margin: 10px 0;
         font-size: xx-large;
+        color: white;
     }
-    .top-num{
+
+    .top-num {
         margin: 10px 0;
     }
 </style>
