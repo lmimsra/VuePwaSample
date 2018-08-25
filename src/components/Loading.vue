506383f2d047392b96b7b7d9a2cf44d6c81cd798
mmr.K <lmimsra07@gmail.com>
2018-08-25 09:00:34 +0900

    ローディング用ページと画像を追加

---
+++ src/components/Loading.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img :src="pack_man"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Loading",
+        data(){
+            return{
+                pack_man : require('../assets/loading.gif'),
+                meat : require('../assets/camp_map_02.png')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
